cfcc2b8a94070f723e8a0dee450af7aef20810e3
iivlev <iivlev@productengine.com>
2017-05-05 21:59:11 +0300

    initial commit

---
+++ .emacs.el
@@ -0,0 +1,33 @@
+(require 'package) ;; You might already have this line
+
+;; Workaround marmalade cert issue
+;; (if (fboundp 'gnutls-available-p)
+;;     (fmakunbound 'gnutls-available-p))
+;; (setq tls-program '("gnutls-cli --tofu -p %p %h")
+;;       imap-ssl-program '("gnutls-cli --tofu -p %p %s")
+;;       smtpmail-stream-type 'starttls
+;;       starttls-extra-arguments '("--tofu")
+;;       )
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/"))
+;; (add-to-list 'package-archives
+;; 	     '("marmalade" . "https://marmalade-repo.org/packages/"))
+(when (< emacs-major-version 24)
+  ;; For important compatibility libraries like cl-lib
+  (add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/")))
+(package-initialize) ;; You might already have this line
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(eval-when-compile
+  (require 'use-package))
+(require 'diminish)
+(require 'bind-key)
+
+(require 'org)
+(org-babel-load-file "~/.pretty.org")
+
+(setq custom-file "~/.custom.el")
+(load custom-file)
+
