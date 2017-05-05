cc03751c9574b1f5f56275cfd5902d883aba6caf
iivlev <iivlev@productengine.com>
2017-05-05 21:59:06 +0300

    initial commit

---
+++ .custom.el
@@ -0,0 +1,27 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(ansi-color-names-vector
+   ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
+ '(bmkp-last-as-first-bookmark-file "~/.emacs.d/bookmarks")
+ '(custom-enabled-themes (quote (leuven)))
+ '(custom-safe-themes
+   (quote
+    ("c7a9a68bd07e38620a5508fef62ec079d274475c8f92d75ed0c33c45fbe306bc" default)))
+ '(menu-bar-mode t)
+ '(org-agenda-files
+   (quote
+    ("~/MEGAsync/emacs.org/work.org" "~/emacs.org/work.org" "~/MEGAsync/emacs.org/info.org" "~/MEGAsync/emacs.org/home/TODO.org" "~/.pretty.org" "~/emacs.org/GTD.org")))
+ '(package-selected-packages
+   (quote
+    (neotree company-jedi jedi yafolding which-key use-package undo-tree test-simple tern switch-window swiper-helm smartscan rtags rainbow-delimiters nlinum monokai-theme magit loc-changes load-relative js2-mode isearch+ ibuffer-vc highlight-symbol help-fns+ helm-swoop helm-projectile helm-ag guide-key flycheck dumb-jump dired-subtree dired-sort-menu desktop+ datetime counsel company color-theme-monokai cmake-ide cider bookmark+ bitbake better-defaults auto-complete annotate ag))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
